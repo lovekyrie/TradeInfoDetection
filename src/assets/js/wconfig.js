0d35ddc86b6246f097ba450c919757b68706f2d5
Duan Yupeng <fengdurant@163.com>
2018-10-15 13:39:28 +0800

    git

--- src/assets/js/wconfig.js
+++
@@ -1,9 +0,0 @@
-function configIcon(){
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = 'http://at.alicdn.com/t/font_779280_rdlgze2e6v.js';
-  document.head.appendChild(script);
-}
-configIcon();
-
-
