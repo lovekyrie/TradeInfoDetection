4ce1e50689b655e0b446c3af21ed0da35e89a33d
Duan Yupeng <fengdurant@163.com>
2018-10-17 18:04:51 +0800

    add tradeinfopc project

---
+++ pc/src/assets/js/init.js
@@ -0,0 +1,46 @@
+// require ('assets/css/index.less');
+ require ('assets/css/common.css');
+
+// require ('assets/css/reset.css');
+// require ('assets/css/help.css');
+// require ('assets/css/mobile.less');
+// require ('assets/css/project.less');
+// import 'assets/js/ScreenAdaptive.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import 'element-ui/lib/theme-default/index.css';
+import ElementUI from 'element-ui';
+
+import "babel-polyfill";// 解决 IE11 Promise对象未定义
+// import axios from 'axios';
+import './vueFilter';  	//全局过滤器
+import {until,reg,app} from 'assets/js/until'
+import {msg,confirm,loading} from 'hero'
+import 'assets/js/wconfig.js';
+import './iconfont'  //字体库
+import './wconfig.js'
+import {Query} from 'assets/js/query'
+
+// import 'assets/js/Ios';
+// import 'assets/js/WebViewJavascriptBridge';
+
+const hostUrl='http://server.haitiandrive.com';
+
+export default{
+  install(Vue){
+    Vue.use(ElementUI)
+    // Vue.prototype.axios = axios;
+    Vue.prototype.hostUrl=hostUrl;
+    Vue.prototype.until = new until();
+    Vue.prototype.app=new app();
+    Vue.prototype.reg = new reg();
+    Vue.use(msg);
+    Vue.use(confirm);
+    Vue.use(loading);
+    Vue.prototype.Query = Query;
+
+  }
+}
+
+
