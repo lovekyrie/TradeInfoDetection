375ec332cc6c90a31b744777cd4cc8545906afd6
Mrlyk <mrlyk@MrlykdeMacBook-Air.local>
2018-10-18 17:55:06 +0800

    登录静态页面制作完成

--- pc/src/views/entry/login/components/App.vue
+++ pc/src/views/entry/login/components/App.vue
@@ -23,15 +23,20 @@
     <form>
       <div><img src="../img/账号.png"><input type="text" placeholder="请输入账号"> </div>
       <div><img src="../img/密码.png"><input type="password" placeholder="请输入密码"> </div>
-      <div><img src="../img/验证码.png"><input> </div>
-      <div><a href="javascript:void (0)">忘记密码？</a></div>
-      <div><button>登录</button><button>取消</button>
+      <div style="margin-bottom: 10px"><img src="../img/验证码.png"><input style="width: 200px" type="text">
+        <span class="modified">
+          <p>TESTAA</p>
+        </span>
+      </div>
+      <div style="margin-top: 10px"><a href="javascript:void (0)">忘记密码？</a></div>
+      <div style="margin-left: 30px"><button class="loginBtn" type="submit">登录</button>
+        <button class="cancelBtn">取消</button>
       </div>
     </form>
 
 
   </div>
-  <!--底部-->
+  <!--页脚-->
   <div id="footer">
     <p>版权所有：宁波贸信检测技术有限公司 信息产业部备案/许可证编号：浙ICP备16007937号</p>
     <p>宁波贸信检测技术服务有限公司&nbsp;&nbsp;&nbsp;&nbsp;地址：宁波市鄞州区富强路555号三、四楼&nbsp;&nbsp;电话：+86-574-89201299</p>
