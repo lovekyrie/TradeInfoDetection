b4d683e5824980228e9945d36c252260cbb14884
Duan Yupeng <fengdurant@163.com>
2018-10-30 22:58:47 +0800

    add page for 10.30 night

---
+++ pc/src/components/addressManage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="address">
+    <div class="address-wrap" :class="{active:selectItem==item}" @click="choose(item)" v-for="(item, index) in addressList" :key="index">
+      <div><span>收货人：{{item.name}}</span></div>
+      <div><span>联系电话：{{item.phone}}</span></div>
+      <div><span>详细地址：{{item.address}}</span></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["addressList"],
+  data() {
+    return {
+      selectItem: null
+    };
+  },
+  methods: {
+    choose(item) {
+      this.selectItem = item;
+    }
+  }
+};
+</script>
+
+<style lang='less'>
+.address {
+  margin-top: 20px;
+  display: -webkit-flex;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  .address-wrap {
+    padding: 20px 20px 30px;
+    width: 375px;
+    display: -webkit-flex;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    border: 1px solid #ccc;
+    > div {
+      width: 100%;
+    }
+  }
+  .active {
+    border-color: #0d55d2;
+  }
+}
+</style>
--- pc/src/views/buyers/reservationservice/app.vue
+++ pc/src/views/buyers/reservationservice/app.vue
@@ -10,16 +10,32 @@
           <span>检测服务订单信息</span>
         </div>
         <div>
-          <span>服务照片</span>
-          <span>服务名称</span>
-          <span>数量</span>
-          <span>联系电话</span>
+          <div>
+            <span>服务照片</span>
+          </div>
+          <div>
+            <span>服务名称</span>
+          </div>
+          <div>
+            <span>数量</span>
+          </div>
+          <div>
+            <span>联系电话</span>
+          </div>
         </div>
         <div>
-          <img :src="reservation" alt="">
-          <span>{{orderInfo.serviceName}}</span>
-          <span>{{orderInfo.count}}</span>
-          <span>{{orderInfo.linkedPhone}}</span>
+          <div>
+            <img :src="reservation" alt="">
+          </div>
+          <div>
+            <span>{{orderInfo.serviceName}}</span>
+          </div>
+          <div>
+            <span>{{orderInfo.count}}</span>
+          </div>
+          <div>
+            <span>{{orderInfo.linkedPhone}}</span>
+          </div>
         </div>
       </div>
       <div class="linked">
@@ -45,9 +61,11 @@
             </el-form-item>
         </el-form>
       </div>
-      <div class="address">
-
-      </div>
+        <address-manage :addressList="addressList"></address-manage>
+        <el-row>
+          <el-button>提交订单</el-button>
+          <el-button>取消</el-button>
+        </el-row>
     </div>
     <div class="footer">
       <trade-footer></trade-footer>
@@ -56,51 +74,176 @@
 </template>
 
 <script>
-import tradeHeader from 'components/tradeHeader'
-import tradeFooter from 'components/tradeFooter'
-import reservation from './images/reservation_03.png'
+import tradeHeader from "components/tradeHeader";
+import tradeFooter from "components/tradeFooter";
+import reservation from "./images/reservation_03.png";
+import addressManage from 'components/addressManage';
 
 export default {
-  data(){
+  data() {
     return {
       reservation,
-      orderInfo:{
-        serviceName:'CFDA食堂餐厅饭店酒店自制餐饮食品检测',
-        count:1,
-        linkedPhone:'0574-88889999',
+      orderInfo: {
+        serviceName: "CFDA食堂餐厅饭店酒店自制餐饮食品检测",
+        count: 1,
+        linkedPhone: "0574-88889999"
       },
-      form:{
-        vendor:'',
-        productName:'',
-        linkedMan:'',
-        linkedPhone:'',
-        desc:'',
-      }
-    }
+      form: {
+        vendor: "",
+        productName: "",
+        linkedMan: "",
+        linkedPhone: "",
+        desc: ""
+      },
+      addressList: [
+        {
+          name: "章三",
+          phone:'13588880000',
+          address:`浙江省宁波市鄞州区首南街道
+                     蝶缘路280号 安邦大厦 2001-1`
+        },
+        {
+          name: "章三",
+          phone:'13588880000',
+          address:`浙江省宁波市鄞州区首南街道
+                     蝶缘路280号 安邦大厦 2001-1`
+        },
+        {
+          name: "章三",
+          phone:'13588880000',
+          address:`浙江省宁波市鄞州区首南街道
+                     蝶缘路280号 安邦大厦 2001-1`
+        },
+      ]
+    };
   },
-  methods:{
-
-  },
-  components:{
+  methods: {},
+  components: {
     tradeHeader,
     tradeFooter,
+    addressManage,
   }
-}
+};
 </script>
 
 <style lang='less'>
-html,body{
-  height: 100%;
+html,
+body {
   width: 100%;
   background-color: #fff;
-  #app{
+  #app {
     width: 100%;
-    .content{
-      width:1200px;
-      margin: 150px auto 130px;
-      .order-info{
-        margin-bottom: 35px;
-        
+    .content {
+      width: 1200px;
+      margin: 130px auto 130px;
+      .order-info {
+        padding-bottom: 35px;
+        display: -webkit-flex;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        border-bottom: 1px solid #ddd;
+        > div {
+          width: 100%;
+          display: -webkit-flex;
+          display: flex;
+          flex-direction: row;
+          flex-wrap: nowrap;
+          justify-content: space-between;
+          align-items: center;
+          &:nth-of-type(1) {
+            margin: 20px 0;
+            > span {
+              &:nth-of-type(1) {
+                width: 10px;
+                height: 25px;
+                background-color: #0d55d2;
+              }
+              &:nth-of-type(2) {
+                width: 98%;
+              }
+            }
+          }
+          &:nth-of-type(2) {
+            padding: 10px 0;
+            background-color: #f1f1f1;
+          }
+          &:nth-of-type(3) {
+            margin: 30px 0;
+          }
+          &:not(:nth-of-type(1)) {
+            > div {
+              &:nth-of-type(1) {
+                width: 10%;
+              }
+              &:nth-of-type(2) {
+                width: 50%;
+              }
+              &:nth-of-type(3) {
+                width: 25%;
+              }
+              &:nth-of-type(4) {
+                width: 15%;
+              }
+            }
+          }
+        }
+      }
+      .linked {
+        padding: 35px 0;
+        border-bottom: 1px solid #ddd;
+        display: -webkit-flex;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        > div {
+          margin-bottom: 20px;
+          &:nth-of-type(1) {
+            width: 100%;
+            display: -webkit-flex;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            justify-content: space-between;
+            align-items: center;
+            > span {
+              &:nth-of-type(1) {
+                width: 10px;
+                height: 25px;
+                background-color: #0d55d2;
+              }
+              &:nth-of-type(2) {
+                width: 98%;
+              }
+            }
+          }
+        }
+        .el-form {
+          width: 100%;
+          display: -webkit-flex;
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          > div {
+            width: 45%;
+            display: -webkit-flex;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            align-items: center;
+            > label {
+              text-align: left;
+              width: 20%;
+            }
+            > div {
+              width: 80%;
+            }
+            textarea {
+              height: 80px;
+            }
+          }
+        }
       }
     }
   }
